e31c0bd3c00232da514a341d51264413f5ad2674
IsabelUllauri1 <iullaurib@est.ups.edu.ec>
2025-11-13 23:07:16 -0500

    agregado lu ultimo

--- src/main/java/com/mycompany/andradevi_ullauriic_prototype/Serpiente.java
+++ src/main/java/com/mycompany/andradevi_ullauriic_prototype/Serpiente.java
@@ -22,6 +22,14 @@ public class Serpiente {
         this.cabezaY = cabezaY;
     }
     
+    public int getCabezaX() {
+        return cabezaX;
+    }
+
+    public int getCabezaY() {
+        return cabezaY;
+    }
+    
     public Segmento getCabeza(){
         //devolvemos lo que se encuentre en la lista de segmentos
         //posicion 0, que es en donde esta la cabeza
@@ -53,11 +61,44 @@ public class Serpiente {
         if(posicionAX < 0 || (posicionAX >= 20) || (posicionAY < 0) || (posicionAY >=20)){
             return;
         }
+        //------------------------------
+        //guarda las posiciones de todos los segmentos
+        ArrayList<int[]> anterior = new ArrayList<>(segmentos.size());
+        for (Segmento s : segmentos) {
+            anterior.add(new int[]{panelTablero.getCeldaElementoX(s), panelTablero.getCeldaElementoY(s)});
+        }
         
         cabezaX = posicionAX;
         cabezaY = posicionAY;
-        
         panelTablero.colocarElementoCelda(getCabeza(), cabezaX, cabezaY);
+        
+        //-----------------------------------
+        //mueve el resto del cuerpo siguiendo al anterior
+        for (int i = 1; i < segmentos.size(); i++) {
+            int[] pos = anterior.get(i - 1);
+            panelTablero.colocarElementoCelda(segmentos.get(i), pos[0], pos[1]);
+        }
     } 
+    
+    //-----------------------------------------
+    // Agrega un segmento nuevo al final en la ultima posicion del cuerpo
+    public void agregarSegmento(PanelTableroSnake panelTablero, Segmento nuevo) {
+
+        //obtiene el último segmento actual de la serpiente
+        int colaIndice = segmentos.size() - 1;
+        Segmento cola = segmentos.get(colaIndice);
+
+       //calcula en qué celda está actualmente la cola
+        int cx = panelTablero.getCeldaElementoX(cola);
+        int cy = panelTablero.getCeldaElementoY(cola);
+
+        //pone el nuevo segmento en esa misma celda
+        //en el siguiente movimiento se pone automaticamente al final
+        panelTablero.colocarElementoCelda(nuevo, cx, cy);
+
+        //pone el nuevo segmento en la lista de partes
+        segmentos.add(nuevo);
+    }
+
     
 }
